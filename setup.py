5f67bdb2230a8eaa5f8a5b1ba2b49132ea563974
Tamal Karmakar <giet13ee080@gmail.com>
2021-05-05 21:13:30 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='model helps to detect fault wafers',
+    author='tamal',
+    license='MIT',
+)
